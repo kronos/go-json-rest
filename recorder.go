25fe8b09901bb1dae633777300ca9c0cd245b0f7
Samsonov Ivan <ivan@wheely.com>
2013-08-16 15:05:13 +0400

    Added hack for websockets

--- recorder.go
+++ recorder.go
@@ -1,6 +1,8 @@
 package rest
 
 import (
+	"bufio"
+	"net"
 	"net/http"
 )
 
@@ -14,6 +16,10 @@ func (self *recorderResponseWriter) WriteHeader(code int) {
 	self.ResponseWriter.WriteHeader(code)
 	self.statusCode = code
 	self.wroteHeader = true
+}
+
+func (self *recorderResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	return self.ResponseWriter.(http.Hijacker).Hijack()
 }
 
 func (self *recorderResponseWriter) Write(b []byte) (int, error) {
